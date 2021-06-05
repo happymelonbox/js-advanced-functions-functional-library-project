9b0ecfbceec8ad710e4c7e1ba44a9efde27f9224
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-05 15:10:41 +1000

    Added some explanation incase I want to return to this for some clarification

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const fi = (function() {
 
     each: function(collection, callback) {
       const arg = Array.isArray(collection) ? collection : Object.values(collection);
+      //Ternary operator to check if collection is an array, if is array
+      //we assign it as it is, if it is and object we use Object.values which returns an array and is assigned to arg.
       for (let i=0; i<arg.length; i++){
         let j = i
         callback(arg[j])
       }
+      //looping through the resulting array(arg) and call the callback function on each value
       return collection
+      // Return the original array
     },
     map: function() {
 
