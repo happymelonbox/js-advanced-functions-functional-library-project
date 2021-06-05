34d503dd87e4195a7003f1844ac22e66ff3cecc8
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-05 15:06:25 +1000

    each complete

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      const arg = Array.isArray(collection) ? collection : Object.values(collection);
+      for (let i=0; i<arg.length; i++){
+        let j = i
+        callback(arg[j])
+      }
+      return collection
     },
-
     map: function() {
 
     },
